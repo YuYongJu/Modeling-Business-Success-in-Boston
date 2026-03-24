7fd0dfb4063b16ef3e315ef7580afc57a68537db
TaylorDunn03 <dunn.tay@northeastern.edu>
2026-03-24 13:58:54 -0400

    Add Yelp API pull script

---
+++ Yelp_API_pull.py
@@ -0,0 +1,12 @@
+'''
+pip install -r requirements.txt
+python sample.py
+python sample.py --term="bars" --location="San Francisco, CA"
+'''
+
+import requests
+
+url = 'yelp.com'
+response = response.get(url)
+
+print(response
