a4366e18bb0c0eb84e5df2336e09724e8c80c713
TaylorDunn03 <dunn.tay@northeastern.edu>
2026-04-22 00:12:20 +0000

    shared zips encoding

--- src/fetch_foodanddrink.py
+++ src/fetch_foodanddrink.py
@@ -9,6 +9,7 @@ Adds calculated age of the business in dataframe
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import shared
 
 FILEPATH = 'data/food_drink_licenses.csv'
 
@@ -17,24 +18,6 @@ EXCLUDED_TYPES = [
         'Lodging Houses (Frat/Dorm)',
         'SPCMWA'
     ]
-
-ZIPS_BY_NEIGHBORHOOD = {
-        'Allston/Brighton' : [2134, 2135, 2163],
-        'Back Bay/Beacon Hill ' : [2108, 2116, 2117, 2123, 2133, 2199, 2216, 2217, 2295],
-        'Central Boston' : [2101, 2102, 2103, 2104, 2105, 2106, 2107, 2109, 2110, 2111, 2112, 2113, 2114, 2196, 2201, 2202, 2203, 2204, 2205, 2206, 2207, 2208, 2209, 2211, 2212, 2222, 2293],
-        'Charlestown ' : [2129],
-        'Dorchester' : [2122, 2124, 2125],
-        'East Boston' : [2128, 2228],
-        'Fenway/Kenmore' : [2115, 2215],
-        'Hyde Park' : [2136],
-        'Jamaica Plain' : [2130],
-        'Mattapan' : [2126],
-        'Roslindale' : [2131],
-        'Roxbury' : [2119, 2120, 2121],
-        'South Boston' : [2127, 2210],
-        'South End' : [2118],
-        'West Roxbury' : [2132]
-    }
 
 def create_businesses(filepath=FILEPATH):
     '''
@@ -84,6 +67,7 @@ def encode_neighborhoods(businesses_df):
     Encode neighborhoods from given zipcodes
     This is important to include because in the raw data a ton are just labelled as "Boston"
     '''
+    zips = shared.ZIPS_BY_NEIGHBORHOOD
     missing_zips = businesses_df[businesses_df['zip'].isna()]
     print(f"{len(missing_zips)} businesses with missing zip codes.")
 
